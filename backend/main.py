38a05a7ee5f36f4d4d31e62704a00c011f36483b
afeefaquadri-png <afeefaquadri@gmail.com>
2026-01-17 23:22:02 +0530

    Fix import paths for routers in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import data_router, model_router
+from routers import data_router, model_router
 from .modules.utils import setup_logging
 import uvicorn
 
